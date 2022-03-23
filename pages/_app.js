f6c08cb5d7f7806c841570d733d3f9f629bafa43
Pedro Silva <pe_lfsilva@hotmail.com>
2022-03-23 17:02:55 -0300

    Calendar styles added

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,33 @@
-import { useEffect } from 'react'
-import { Provider } from 'react-redux'
-import store from '../store'
+import { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import store from "../store";
 import "slick-carousel/slick/slick.scss";
-import 'react-modal-video/scss/modal-video.scss'
-import '../styles/style.scss';
-
-
-// compoennst 
-import HeaderBottom from '../components/HeaderBottom';
-import FooterArea from '../components/FooterArea';
+import "react-modal-video/scss/modal-video.scss";
+import "../styles/style.scss";
+import HeaderBottom from "../components/HeaderBottom";
+import FooterArea from "../components/FooterArea";
 
 const MyApp = ({ Component, pageProps }) => {
-    return (
-        <Provider store={store}>
-            <header className="headerArea">
-                <HeaderBottom className="headerBottomArea" />
-            </header>
+  const [loading, setLoading] = useState(true);
 
-            <Component {...pageProps} />
-            <FooterArea />
-        </Provider>
-    )
-}
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
-export default MyApp
+  if (loading) {
+    return <></>;
+  }
+
+  return (
+    <Provider store={store}>
+      <header className="headerArea">
+        <HeaderBottom className="headerBottomArea" />
+      </header>
+
+      <Component {...pageProps} />
+      <FooterArea />
+    </Provider>
+  );
+};
+
+export default MyApp;
